8345453924ee2b85672103724cb76157692e8098
Denis <incinerator88@mail.ru>
2023-01-31 16:11:38 +0300

    Update elements_page.py, elements_page_locators.py, added new test "TestRadioButton"

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -29,4 +29,19 @@ class TestElements:
             assert input_checkbox == output_result, 'checkboxes have not been selected'
 
 
+    class TestRadioButton:
+
+        def test_radio_button(self, driver):
+            radio_button_page = RadioButtonPage(driver, "https://demoqa.com/radio-button")
+            radio_button_page.open()
+            radio_button_page.click_on_the_radio_button('yes')
+            output_yes = radio_button_page.get_output_result()
+            print(output_yes)
+            radio_button_page.click_on_the_radio_button('impressive')
+            output_impressive = radio_button_page.get_output_result()
+            radio_button_page.click_on_the_radio_button('no')
+            output_no = radio_button_page.get_output_result()
+            assert output_yes == 'Yes', "YES have not been selected"
+            assert output_impressive == 'Impressive', "IMPRESSIVE have not been selected"
+            assert output_no == 'No', "NO have not been selected"
 
